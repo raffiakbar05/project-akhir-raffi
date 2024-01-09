ef92fd0d289645f2b62a944dc09a12cae90e7a2b
raffiakbar05 <akbarbaihaqy0@gmail.com>
2024-01-09 11:14:57 +0700

    first

---
+++ raffi.php
@@ -0,0 +1,7 @@
+<?php
+//menampilkan string
+$Name = " Raffi Akbar";
+echo "nama saya adalah" . $Name;
+echo "<br>";
+$Absen = 2;
+print "Saya absen" $Absen;
---
+++ tugas.php
@@ -0,0 +1,7 @@
+<?php
+//MENAMPILKAN STRING
+$Nama = " Raffi";
+echo " Nama saya adalah" . $Nama;
+echo "<br>";
+$Absen = " 02"
+echo "Absen saya " . $Absen;
